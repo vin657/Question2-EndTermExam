c5bf32738e4ca27cdfc0a25d877eede5154b6820
vineet <vineet@LAPTOP-R4RMOVHA>
2019-08-02 13:35:04 +0530

    Done

--- src/main/java/main/Question2.java
+++ src/main/java/main/Question2.java
@@ -20,8 +20,9 @@ public static void main(String[] args) {
 
 }
 public List<Integer> listWithDuplicates(List<Integer> number){
-	List <Integer> L1 = null;
-	//write your code here !! change the L1 to required value
+	List <Integer> L1 = new ArrayList<Integer>();
+	//write your code here !! change the L1 to required value 
+	L1.addAll(number);
 	return L1 ;
 }
 }
